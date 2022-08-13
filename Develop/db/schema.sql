6b88d5afb08b53814ecb95ab9935d741a3654b0b
Mina Ghaly <minanabil@msn.com>
2022-08-13 23:51:18 +0200

    added sql queries

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,23 +1,24 @@
+DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
 CREATE TABLE department(
-id INT NOT NULL PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 name VARCHAR(30));
 
 CREATE TABLE role(
-id INT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY , 
 title VARCHAR(30), 
 salary DECIMAL,
-department_id INT)
+department_id INT);
 
 CREATE TABLE employee(
-id INT PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
-manager_id INT )
+manager_id INT );
 -- to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 
 
--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,53 +1,152 @@
 const { query } = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-function show(myQuery){
-const myconnection = mysql.createConnection({
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // MySQL password
-    password: 'P@ssw0rd',
-    database: 'employee_tracker'
-  },
-  console.log(`Connected to the courses_db database.`)
-);
-myconnection.query(myQuery,function(error,results){console.log(results)})
+
+
+function show(myQuery) {
+    const myconnection = mysql.createConnection({
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'P@ssw0rd',
+        database: 'employee_tracker'
+    },
+        console.log(`Connected to the courses_db database.`)
+    );
+    myconnection.query(myQuery, function (error, results) {
+        console.log(results)
+    })
 }
-//WHEN I start the application
-//THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+
 
 const intro_question = [{
     type: 'list',
     name: 'introquestions',
     message: 'Please add other team members, otherwise please exit',
-    choices: ['view all departments', 'view all roles', 'view all employees','add a department','add a role','add an employee','update an employee role']
+    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
 }]
 
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
 
-inquirer.prompt(intro_question).then(answers=>{
-switch (answers.introquestions){
-     case 'view all departments':
-        show('select * FROM department');
-        break;
-     case 'view all roles':
-        show('SELECT role.title,role.id,department.name,role.salary FROM role JOIN department ON role.id = department.id');
-
-        //to be continued
-    case 'all employees':
-        show(` SELECT employee.id,employee.first_name,employee.last_name,role.title,department.name,role.salary,employee.name AS manager
+inquirer.prompt(intro_question).then(answers => {
+    switch (answers.introquestions) {
+        case 'View All Departments':
+            show(`SELECT * 
+           FROM department`)
+            break;
+        case 'View All Roles':
+            show(`SELECT role.id,role.title, department.name,role.salary 
+            FROM role 
+            JOIN department ON role.id = department.id`);
+            break;
+        case 'View All Employees':
+            show(`SELECT employee.id,employee.first_name,employee.last_name,role.title,department.name,role.salary,CONCAT(manager.first_name,manager.last_name) AS manager
         FROM employee
-        JOIN role ON employee.id = role.id
-        JOIN department ON employee.id = department.`)
-}
-})
+        JOIN role
+        ON employee.role_id = role.id
+        JOIN department
+        ON role.department_id = department.id)
+        INNER JOIN employee manager
+        ON employee.manager_id = manager.id`)
 
+            break;
+        case 'Add Department':
+            inquirer.prompt([{
+                name: "department_name",
+                message: "What is the name of the department?"
+            }]).then(answers => {
+                show(`INSERT INTO department (name)
+                VALUES ("${answers.department_name}"); `)
+                console.log(`Added ${answers.department_name} to the database`);
+            })
+            break;
+        case 'Add Role':
+            inquirer.prompt([{
+                name: "roleName",
+                message: "What is the name of the role?"
+            },
+            {
+                name: "salary",
+                message: "What is the salary of the role?"
+            },
+            {
+                type: 'list',
+                name: "department",
+                message: "Which department does the role belong to?",
+
+                //TO BE FIXED
+                choices: ['Engineering', 'Finance', 'Legal', 'Sales', 'Service']
+            }
+            ]).then(
+                answers => {
+                    const departmentName = show(`SELECT id
+                             FROM department
+                             WHERE name = ${answers.department}`);
+
+                    show(`INSERT INTO role(title,salary,department_id)
+                         VALUES("${answers.roleName}","${answers.salary}", "${departmentName}")`)
+                    console.log(`Added ${departmentName} to the database`);
+                }
+            )
+            break;
+        case 'Add Employee':
+            inquirer.prompt([{
+                name: 'employeeFname',
+                message: 'What is the employee\'s first name?'
+            },
+            {
+                name: 'employeeLname',
+                message: 'What is the employee\'s last name?'
+            },
+            {
+                type: 'list',
+                name: 'employeeRole',
+                message: 'What is the employee\'s role?',
+
+                //to be fixed
+                choices: ['Sales Lead', 'Sales Person', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer', 'Customer Service', 'Sales Lead', 'Salesperson', 'Lead Engineer']
+            },
+            {
+                type: 'list',
+                name: 'manager',
+                message: 'What is the employee\'s manager?',
+                //TO BE FIXED ADD NONE
+                choices: ['mina', 'nabil']
+            }
+
+            ]).then(
+                answers => {
+                    const roleId = show(`SELECT role.id
+                        FROM role
+                        WHERE role.title = answers.employeeRole}`)
+                    const managerId = show(`
+                         SELECT employee.id
+                         FROM employee
+                         WHERE answers.manager = CONCAT(employee.first_name,employee.last_name)`)
+                    show(
+                        //To be fixed role id and manager id
+                         `INSERT INTO employee(first_name,last_name,role_id,manager_id)
+                         VALUES ("${answers.employeeFname}", "${answers.employeeLname}","1", 
+                            "2");`)
+
+                    console.log(answers.employeeFname + ' ' + answers.employeeLname + " " + roleId + " " + managerId);
+                }
+            )
+            break;
+        case 'Update Employee Role':
+            inquirer.prompt([{
+                type: 'list',
+                name: 'employeeRole',
+                message: 'Which employee\'s role do you want to update?',
+                choices: [show(`SELECT CONCAT(employee.first_name,employee.last_name)
+                FROM employee`)]
+            }
+
+            ])
+    }
+}
+)
 
 
