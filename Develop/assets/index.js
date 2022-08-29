fe805ab9e25d21288e38155168bfcca90b2dbfd5
Mina Ghaly <minanabil@msn.com>
2022-08-29 11:28:35 +1000

    manager update feature added

--- Develop/assets/index.js
+++ Develop/assets/index.js
@@ -20,9 +20,9 @@ function init() {
 }
 
 function show(myQuery) {
-        myconnection.query(myQuery, (err, res) => {
-        if(err)console.log(err);
-        if (myQuery[0] == 'S') { console.log(res) };
+    myconnection.query(myQuery, (err, res) => {
+        if (err) console.log(err);
+        if (myQuery[0] == 'S') { console.table(res) };
         prompt();
     })
 }
@@ -36,19 +36,19 @@ const intro_question = [{
     type: 'list',
     name: 'introquestions',
     message: 'Please add other team members, otherwise please exit',
-    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Update Employee Manager', 'Quit']
 }]
 
 
 function prompt() {
     inquirer.prompt(intro_question)
-    //we added async in front of a function here, so that we can use await many times inside that function
-    //using myconnection.promise.query
+        //we added async in front of a function here, so that we can use await many times inside that function
+        //using myconnection.promise.query
         .then(async answers => {
             switch (answers.introquestions) {
-               case 'Quit':
-                console.log("Goodbye");
-                process.exit();
+                case 'Quit':
+                    console.log("Goodbye");
+                    process.exit();
 
                 case 'View All Departments':
                     show(`SELECT * 
@@ -62,7 +62,7 @@ function prompt() {
             JOIN department ON role.id = department.id`);
                     break;
 
-                case 'View All Employees':                
+                case 'View All Employees':
                     show(`SELECT employee.id,
                     employee.first_name,
                     employee.last_name,
@@ -86,144 +86,162 @@ function prompt() {
                         console.log(`Added ${answers.department_name} to the database`);
                     })
                     break;
-                    //try
+                //try
                 case 'Add Role':
-                    //await to be used only with promise objects.
-                    //asynchronous functions are based on either cb functions or promise object
-                    //myconnection is async, but it is not based on promises. 
-                    //myconnection.promise().query is async based on promises, so we dont need this callback function
-                    //(err,res)=>{return res} and will be using either async/await or then
-                // const departmentChoices =  await myconnection.promise().query(`SELECT * FROM department`);
-                // console.log(departmentChoices);
-                //myconnection.promise since based on promises, we can use await, however to use await, will have to put it inside 
-                //async function.
-                //it does return an array of arrays, the first array is the table we would like to retrieve
-                const [departments] =  await myconnection.promise().query(`SELECT * FROM department`);
-                const departmentChoices = departments.map(el => ({name: el.name, value: el.id}))
-                            inquirer.prompt([{
-                            name: "roleName",
-                            message: "What is the name of the role?"
-                        },
-                        {
-                            name: "salary",
-                            message: "What is the salary of the role?"
-                        },
-                        {
-                            type: 'list',
-                            name: "department",
-                            message: "Which department does the role belong to?",
-                            choices: departmentChoices
-                        }
-                        ]).then(
-                            answers => {
-                        
-                                show(`INSERT INTO role(title,salary,department_id)
-                                 VALUES("${answers.roleName}","${answers.salary}", "${answers.department}")`)
-                                console.log(`Added ${answers.roleName} to the database`);
-                            }
-                        )
-                    
-                    break;      
-                    case 'Add Employee':
-                        const [roles] = await myconnection.promise().query(`SELECT *
-                        FROM role;`)
-                        const roleChoices = roles.map(el => ({name: el.title, value: el.id}));
-                        const [manager] = await myconnection.promise().query(` SELECT *
-                        FROM employee;`);
-                        let managerList = [{}];
-                         managerList = manager.map(el => ({name: el.first_name+' '+el.last_name, value: el.id}));
-                        if(managerList.length === 0){managerList.push({name:"no manager", value: "0"})};
-                        
-                        
-                        inquirer.prompt([{
-                            name: 'employeeFname',
-                            message: 'What is the employee\'s first name?'
-                        },
-                        {
-                            name: 'employeeLname',
-                            message: 'What is the employee\'s last name?'
-                        },
-                        {
-                            type: 'list',
-                            name: 'employeeRole',
-                            message: 'What is the employee\'s role?',
-                            choices: roleChoices
-                        },
-                        {
-                            type: 'list',
-                            name: 'manager',
-                            message: 'What is the employee\'s manager?',
-                            choices: managerList
-                        }
-                        
+                    const [departments] = await myconnection.promise().query(`SELECT * FROM department`);
+                    const departmentChoices = departments.map(el => ({ name: el.name, value: el.id }))
+                    inquirer.prompt([{
+                        name: "roleName",
+                        message: "What is the name of the role?"
+                    },
+                    {
+                        name: "salary",
+                        message: "What is the salary of the role?"
+                    },
+                    {
+                        type: 'list',
+                        name: "department",
+                        message: "Which department does the role belong to?",
+                        choices: departmentChoices
+                    }
                     ]).then(
                         answers => {
-                            
+
+                            show(`INSERT INTO role(title,salary,department_id)
+                                 VALUES("${answers.roleName}","${answers.salary}", "${answers.department}")`)
+                            console.log(`Added ${answers.roleName} to the database`);
+                        }
+                    )
+
+                    break;
+                case 'Add Employee': {
+                    const [roles] = await myconnection.promise().query(`SELECT *
+                        FROM role;`)
+                    const roleChoices = roles.map(el => ({ name: el.title, value: el.id }));
+                    const [manager] = await myconnection.promise().query(` SELECT *
+                        FROM employee;`);
+                    let managerList = [{}];
+                    managerList = manager.map(el => ({ name: el.first_name + ' ' + el.last_name, value: el.id }));
+                    if (managerList.length === 0) { managerList.push({ name: "no manager", value: "0" }) };
+
+
+                    inquirer.prompt([{
+                        name: 'employeeFname',
+                        message: 'What is the employee\'s first name?'
+                    },
+                    {
+                        name: 'employeeLname',
+                        message: 'What is the employee\'s last name?'
+                    },
+                    {
+                        type: 'list',
+                        name: 'employeeRole',
+                        message: 'What is the employee\'s role?',
+                        choices: roleChoices
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: 'What is the employee\'s manager?',
+                        choices: managerList
+                    }
+
+                    ]).then(
+                        answers => {
+
                             show(
                                 `INSERT INTO employee(first_name,last_name,role_id,manager_id)
                                 VALUES ("${answers.employeeFname}", "${answers.employeeLname}",${answers.employeeRole}, 
                                 ${answers.manager});`)
-                                
-                                console.log(answers.employeeFname + ' ' + answers.employeeLname + " has been added");
-                            }
-                            )
-                            break;
-                            case 'Update Employee Role':{
-                                const [roles] = await myconnection.promise().query(`SELECT *
-                                FROM role;`)
-                                const roleChoices = roles.map(el => ({name: el.title, value: el.id}));
-                                const [manager] = await myconnection.promise().query(` SELECT *
+
+                            console.log(answers.employeeFname + ' ' + answers.employeeLname + " has been added");
+                        }
+                    )
+                    break;
+                }
+                case 'Update Employee Role': {
+                    const [roles] = await myconnection.promise().query(`SELECT *
+                                FROM role;`);
+                    const roleChoices = roles.map(el => ({ name: el.title, value: el.id }));
+                    const [manager] = await myconnection.promise().query(` SELECT *
                                 FROM employee;`);
-                                const managerList = manager.map(el => ({name: el.first_name+' '+el.last_name, value: el.id}))
-                               
-//employee names and their ids
-                                inquirer.prompt([{
-                                    type: 'list',
-                                    name: 'employee',
-                                    message: 'Which employee\'s role do you want to update?',
-                                    choices: managerList
-                                                                }
-                                //role title and role id
-                            ]).then(answers1=>{
-                                inquirer.prompt([{
-                                    type: 'list',
-                                    name: 'role',
-                                    message: 'Which role do you want to update?',
-                                    choices: roleChoices
-                                                                }
-                                
-                            ]).then(
-                                answers2=>{show(
-                                `UPDATE employee
+                    const managerList = manager.map(el => ({ name: el.first_name + ' ' + el.last_name, value: el.id }))
+
+                    //employee names and their ids
+                    inquirer.prompt([{
+                        type: 'list',
+                        name: 'employee',
+                        message: 'Which employee\'s role do you want to update?',
+                        choices: managerList
+                    }
+                        //role title and role id
+                    ]).then(answers1 => {
+                        inquirer.prompt([{
+                            type: 'list',
+                            name: 'role',
+                            message: 'Which role do you want to update?',
+                            choices: roleChoices
+                        }
+
+                        ]).then(
+                            answers2 => {
+                                show(
+                                    `UPDATE employee
                                 SET role_id = ${answers2.role}
                                 WHERE id = ${answers1.employee}
                                 `
-
-
                                 )
-                                
                                 console.log("role has been updated successfully")
-                                
-                                }
-
-                            )
-
-
-
                             }
+                        )
 
-                            )
-                            break;
-                        }
-                            
-                            
-                        }
+                    }
 
-                        
+                    )
+                    break;
+                }
+
+                case 'Update Employee Manager': {
+                    const [employeeList] = await myconnection.promise().query(
+                        `SELECT  first_name,last_name,id
+                        FROM employee;
+                        `
+                    );
+
+                    const employee = employeeList.map(el => ({ name: el.first_name + ' ' + el.last_name, value: el.id }))
+
+                    inquirer.prompt([{
+                        type: 'list',
+                        name: 'employee',
+                        message: 'Which employee do you want to update?',
+                        choices: employee
+                    },
+                    {
+
+                        type: 'list',
+                        name: 'manager',
+                        message: 'Which manager do you want to choose?',
+                        choices: employee
+                    }
+
+                        //role title and role id
+                    ]).then(answers => {
+
+                        console.log('manager has been updated');
+                        show(`
+                        UPDATE employee
+                        SET manager_id = ${answers.manager}
+                        WHERE id = ${answers.employee};
+                        `
+                        )
                     }
                     )
-                    
+
                 }
-                
-                
+            }
+        }
+        )
+
+}
 init();
+
